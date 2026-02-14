1c1b6344a54d77036824a440a1323bcc123a9b1c
Curt Zirzow <czirzow@gmail.com>
2026-02-13 22:06:36 -0800

    I give my self an solid A on this one.

--- day24/Mail Merge Project Start/main.py
+++ day24/Mail Merge Project Start/main.py
@@ -2,7 +2,27 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+starting_letters_file = "Input/Letters/starting_letter.txt"
+invited_names_file = "Input/Names/invited_names.txt"
+ready_to_send_dir = "Output/ReadyToSend/"
+
+
+# Get the list of invited names
+invited_names = []
+with open(invited_names_file) as f:
+    # as we iterate the file lines, clean them up a little.
+    invited_names = [ name.strip() for name in f ]
+
+# set up the template
+template = ""
+with open(starting_letters_file) as f:
+    template = f.read()
+
+for name in invited_names:
+    output_file = f"{ready_to_send_dir}{name}.txt"
+    with open(output_file, mode='w') as output:
+        # replace the text '[name]' with {name}
+        output.write(template.replace('[name]', name))
+
