01754456385889023fd5b85170901bca5b63fdaa
Curt Zirzow <czirzow@gmail.com>
2026-02-16 14:32:32 -0800

    Notes about pomodoro

--- day28/notes.py
+++ day28/notes.py
@@ -1,1 +1,12 @@
 # notes
+#
+# The Pomodoro Technique
+# ======================
+# Do 4 times:
+#     1. Decide on the task to be done
+#     2. Set timer to 25 minutes
+#     3. Work on the task until the timer rings
+#     4. Take a short 5 minute break
+# 5. take 15-30 minute break
+
+
