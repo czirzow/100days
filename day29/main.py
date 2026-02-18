36eccf9fb17c72d0a0c4c72410945ed3fb6f9344
Curt Zirzow <czirzow@gmail.com>
2026-02-17 21:17:56 -0800

    change location of password file.

--- day29/main.py
+++ day29/main.py
@@ -5,7 +5,7 @@ from tkinter import *
 
 #Global:
 widgets = {}
-file_name = 'passwords/data.txt'
+file_name = 'data/passwords.txt'
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
