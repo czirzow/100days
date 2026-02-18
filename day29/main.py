5562c11bdc3344efe06f6bc620deec1bf3e02b56
Curt Zirzow <czirzow@gmail.com>
2026-02-17 23:31:44 -0800

    fixed a bug. and we generate a random password

--- day29/main.py
+++ day29/main.py
@@ -13,8 +13,34 @@ widgets = {}
 file_name = 'data/passwords.txt'
 
 
+from random import choice, randint, shuffle, shuffle
+class RandomPassword():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    @classmethod
+    def get(cls):
+        """ returns a random password """
+
+        password_list  = [choice(cls.letters) for _ in range(randint(8, 10))]
+        password_list += [choice(cls.numbers) for _ in range(randint(2, 4))]
+        password_list += [choice(cls.symbols) for _ in range(randint(2, 4))]
+
+        shuffle(password_list)
+
+        return "".join(password_list)
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
+    global widgets
+
+    # FIXEDME: this isn't working!!!
+    # thanks to angela... i was stuck on this.
+    widgets['password'].insert(END, RandomPassword.get())
+
+
     pass
 
 
