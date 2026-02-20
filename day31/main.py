1a4fefa3c8752c7b4a88bdb36f92536ed59e490a
Curt Zirzow <czirzow@gmail.com>
2026-02-19 20:42:13 -0800

    init.

---
+++ day31/main.py
@@ -0,0 +1,3 @@
+# day 31
+#force pycharm to read requirements.txt
+import panda
---
+++ day31/notes.py
@@ -0,0 +1,24 @@
+# notes
+# see what i have written down
+
+# . flash card game to learn a language.
+# . frequency dictionary:
+#   . Task: do language_codes ['fr', 'de', 'es']
+#   . i don't know french at all.
+
+
+# My plan:
+# cheat.
+# 
+# get the answer. and run it and see how it works.
+# Review:
+#   . flow of app.
+#   . look for freatures i would like to add.
+#   . make a plan for each feature to add.
+
+# Featurers:
+#   . Add a level of knowlege: 
+#   . define [ 'beginner', 'intermediate', 'expert'] like list 
+#   . choose language.
+
+
