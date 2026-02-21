fbead125779573ad0074f0725b03c474f5dd182f
Curt Zirzow <czirzow@gmail.com>
2026-02-21 12:08:30 -0800

    Mostly works. a few glitches.

--- day31/main.py
+++ day31/main.py
@@ -1,11 +1,8 @@
 # day 31
 import tkinter as tk
+from lib31 import translator as t
 
 
-LANGUAGES = {
-        'fr': 'French',
-        'en': 'English',
-        }
 CARD_IMAGES = {
         'front': 'images/card_front.png',
         'back': 'images/card_back.png',
@@ -40,6 +37,70 @@ def setup_widgets(parent: tk.Tk, widgets: dict, layout: dict):
 
     return widgets
 
+class Flashy():
+    def __init__(self, front_img, back_img):
+        self.front_img = front_img
+        self.back_img = back_img
+        self.timer_id = ""
+
+    def set_widgets(self, widgets):
+        self.widgets = widgets
+
+    def set_translator(self, translator):
+        self.translator = translator
+
+    def set_buttons(self):
+        self.widgets['press_right'].config(command=self.press_right)
+        self.widgets['press_wrong'].config(command=self.press_wrong)
+        pass
+
+
+    def press_right(self):
+        if self.timer_id:
+            return
+        self.translator.remove(self.lang_from)
+        self.new_card()
+
+    def press_wrong(self):
+        if self.timer_id:
+            return
+        self.show_back()
+        self.count_down(5)
+
+    def new_card(self):
+        (self.lang_from, self.lang_to) = self.translator.get()
+        self.show_front()
+        print(self.lang_from, self.lang_to)
+
+    def show_front(self):
+        self.widgets['top_label'].config(text=self.translator.lang['from'])
+        self.widgets['bottom_label'].config(text=self.lang_from)
+        self.widgets['card_front'].config(image=self.front_img)
+
+    def show_back(self):
+        self.widgets['top_label'].config(text=self.translator.lang['to'])
+        self.widgets['bottom_label'].config(text=self.lang_to)
+        self.widgets['card_front'].config(image=self.back_img)
+
+    def start_timer(self):
+        pass
+
+    def finish_timer(self):
+        window.after_cancel(self.timer_id)
+        self.timer_id = ""
+        self.new_card()
+
+
+    def count_down(self, count):
+        if count > 0:
+            print("count: ", count)
+            self.timer_id = window.after(1000, self.count_down, count - 1)
+        else:
+            self.finish_timer()
+
+
+
+
 
 window = tk.Tk()
 window.title("Flashy")
@@ -49,15 +110,20 @@ window.config(bg=BG_COLOR,
 window.resizable(False, False)
 
 
+flashy = Flashy(front_img=tk.PhotoImage(file=CARD_IMAGES['front']),
+                back_img=tk.PhotoImage(file=CARD_IMAGES['back']))
+flashy.set_translator(t.Translator('fr', 'en'))
+
 # 
 # layout: a dict for setup widgets(layout=layout) 
-# TODO: add font and add the two buttons at the bottom.
-# TODO: add card_back it's basicly a dup of 'front' but with a different image
 layout = {
         'card_front': {
+            # Front of card
             'type': tk.Button,
             'config': {
                 'image': tk.PhotoImage(file=CARD_IMAGES['front']),
+                'height': 526,
+                'width': 800,
                 'bg': BG_COLOR,
                 'relief': 'flat',
                 'activebackground': BG_COLOR,
@@ -69,7 +135,7 @@ layout = {
                 'columnspan': 2,
                 },
             'children': {
-                'front_lang_from': {
+                'top_label': {
                     'type': tk.Label,
                     'config': {
                         'text': 'French',
@@ -81,7 +147,7 @@ layout = {
                         'y': 150,
                         },
                     }, #/front_lang_from
-                'front_lang_to': {
+                'bottom_label': {
                     'type': tk.Label,
                     'config': {
                         'text': 'Français',
@@ -94,9 +160,7 @@ layout = {
                         },
                     }, #/front_lang_to
                 }, #/children
-            }, #/card front
-        # TODO: add card_back
-        #
+            }, #/card
         'press_wrong': {
             'type': tk.Button,
             'config': {
@@ -117,12 +181,16 @@ layout = {
                 'row': 1,
                 },
             },
-
         }
 
 
 widgets = {}
 setup_widgets(window, widgets, layout)
+
+flashy.set_widgets(widgets)
+flashy.set_buttons()
+
+flashy.new_card()
 
 window.mainloop()
 
