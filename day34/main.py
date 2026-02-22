3338ca4d3ba69d3f5fa6ce2f5df2b9ba67dd0a7c
Curt Zirzow <czirzow@gmail.com>
2026-02-22 15:12:41 -0800

    updates

--- day34/main.py
+++ day34/main.py
@@ -3,18 +3,43 @@ from lib34.quiz_brain import QuizBrain
 
 from data.data import question_data
 
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+from opentrivia.api import OpenTriviaApi
+import json
 
 
-quiz = QuizBrain(question_bank)
+def refresh_data():
+    api = OpenTriviaApi(amount=1)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+    result = api.get()
+    with open('data/data.py', 'w') as fh:
+        print("refresh")
+        fh.write("question_data = ")
+        json.dump(result, fh, indent=4)
+
+    return result
+
+
+def get_question_bank(question_data):
+    question_bank = []
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+
+    return question_bank
+
+
+quiz = QuizBrain(get_question_bank(question_data))
+
+while True:
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print("refreshing data bank")
+    question_data = refresh_data()
+    quiz = QuizBrain(get_question_bank(question_data))
+
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- day34/opentrivia/api.py
+++ day34/opentrivia/api.py
@@ -1,14 +1,48 @@
 import requests as req
+import html
 
 API_ENDPOINT = 'https://opentdb.com/api.php'
 
 class OpenTriviaApi:
     """Retrieve questions from Open Trivia Api"""
 
-    def __init__(self, amount=10, type='boolean', category=0):
+    def __init__(self, amount=10, answer_type='boolean', category=0):
         self.amount = amount
-        self.type = type
+        self.answer_type = answer_type
         self.category = category
 
+    def get_params(self):
+        params = {}
+        
+        if self.amount < 1:
+            self.amount = 10
+        params['amount'] = self.amount
+        
+        if self.answer_type:
+            params['type'] = self.answer_type
+
+        return params
+
     def get(self):
-        pass
+        resp = req.get(API_ENDPOINT, self.get_params())
+        try:
+            resp.raise_for_status()
+
+        except req.exceptions.RequestException as e:
+            print(f"Failed api call: {e}")
+            return {}
+
+        data = resp.json()
+        if data['response_code'] != 0:
+            print("error getting content: code[{data['response_code']}")
+            return {}
+
+        # using default encoding html codes
+        # lets clean it up.
+        for item in data['result']:
+            q = item.get('question')
+            if isinstance(q, str):
+                item['question'] = html.unescape(q)
+
+        return data['results']
+
