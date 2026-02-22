50d048995ef73c06b0f934324174c05bfa27c333
Curt Zirzow <czirzow@gmail.com>
2026-02-21 21:14:42 -0800

    Lets try to recover from an exception.

--- day32/lib32/mailer.py
+++ day32/lib32/mailer.py
@@ -8,12 +8,20 @@ password='qqgt vlhb oorr ldwr'
 # this is just proof of concept code.
 __ENABLED__ = False
 
-def sendemail(to, subject, message):
-    with smtplib.SMTP_SSL(smtp_hostname) as smtp:
-        smtp.login(user=my_email, password=password)
-        if __ENABLED__:
-            smtp.sendmail(from_addr=my_email, to_addrs=to,
-                          msg=f"Subject: {subject}\n\n{message}")
-        else:
-            print(smtp.noop())
+def sendemail(to:str, subject:str, message:str) -> bool:
+    """returns false if something went wrong"""
+    try:
+        with smtplib.SMTP_SSL(smtp_hostname) as smtp:
+            smtp.login(user=my_email, password=password)
+            if __ENABLED__:
+                print(f"sending mail to {to}")
+                smtp.sendmail(from_addr=my_email, to_addrs=to,
+                              msg=f"Subject: {subject}\n\n{message}")
+            else:
+                print("noop response:", smtp.noop())
+    except smtplib.SMTPException as e:
+        print(f"Unable to sendmail: {e}")
+        return False
+    else:
+        return True
 
