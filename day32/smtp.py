169a4b8bdd28f3109aea4d304c5fac75da1cb605
Curt Zirzow <czirzow@gmail.com>
2026-02-21 21:19:26 -0800

    cleanup

--- day32/smtp.py
+++ day32/smtp.py
@@ -1,15 +1,3 @@
-import smtplib
-
-smtp_hostname='smtp.gmail.com'
-email='czirzow@gmail.com'
-password='qqgt vlhb oorr ldwr'
-
-def send_email(to, message):
-    if 0:
-        with smtplib.SMTP_SSL(smtp_hostname) as smtp:
-            smtp.login(user=email, password=password)
-            # can we do an operation?
-            print(smtp.noop())
 
 import datetime as dt
 import random as rand
