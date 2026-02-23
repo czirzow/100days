8477c3a872cb8ca426bf7a2a76bfedcff4bd24aa
Curt Zirzow <czirzow@gmail.com>
2026-02-22 17:50:39 -0800

    some bug fixes.

--- day34/lib34/ui.py
+++ day34/lib34/ui.py
@@ -3,6 +3,8 @@ from lib34.quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 COLOR_WHITE = "#FFFFFF"
+COLOR_RED = '#FFB3B3'
+COLOR_GREEN = '#90EE90'
 QUESTION_FONT = ('Arial', 15, 'italic')
 CANVAS_HEIGHT = 250
 CANVAS_WIDTH  = 300
@@ -49,32 +51,40 @@ class QuizInterface:
         self.window.mainloop()
 
     def on_btn_yep(self):
+        self.set_yep_nope('disabled')
         self.give_feedback(self.quiz.check_answer('True'))
-        self.update_score()
 
     def on_btn_nope(self):
+        self.set_yep_nope('disabled')
         self.give_feedback(self.quiz.check_answer('False'))
+
+    def set_yep_nope(self, state):
+        self.btn_yep.config(state=state)
+        self.btn_nope.config(state=state)
+
 
     def update_score(self):
         self.score.config(text=f"Score: {self.quiz.score}")
 
 
     def do_we_want_more(self):
+        self.set_yep_nope('disabled')
         self.canvas.itemconfig(self.question, text="Game Over.")
 
     def give_feedback(self, is_right):
         if is_right:
-            self.canvas.config(bg='green')
+            self.canvas.config(bg=COLOR_GREEN)
         else:
-            self.canvas.config(bg='red')
+            self.canvas.config(bg=COLOR_RED)
         self.window.after(1000, self.get_next_question)
+        self.update_score()
+        self.set_yep_nope('normal')
 
     def get_next_question(self):
         self.canvas.config(bg=COLOR_WHITE)
         if not self.quiz.still_has_questions():
             self.do_we_want_more()
         else:
-
             question = self.quiz.next_question()
             self.canvas.itemconfig(self.question, text=question)
 
