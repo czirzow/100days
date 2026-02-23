c65fcad9fffd5a49d117d2172a00417b117842b4
Curt Zirzow <czirzow@gmail.com>
2026-02-22 17:54:11 -0800

    cleaning up.

--- day34/main.py
+++ day34/main.py
@@ -1,30 +1,11 @@
 from lib34.question_model import Question
 from lib34.quiz_brain import QuizBrain
-
-from data.data import question_data
-
-from opentrivia.api import OpenTriviaApi
-import json
 from lib34.ui import QuizInterface
+from data.manager import Manager
 
 
 
-
-
-
-def refresh_data():
-    api = OpenTriviaApi(amount=1)
-
-    result = api.get()
-    with open('data/data.py', 'w') as fh:
-        print("refresh")
-        fh.write("question_data = ")
-        json.dump(result, fh, indent=4)
-
-    return result
-
-
-def get_question_bank(question_data):
+def to_quiz_brain_data(question_data):
     question_bank = []
     for question in question_data:
         question_text = question["question"]
@@ -35,23 +16,10 @@ def get_question_bank(question_data):
     return question_bank
 
 
-quiz = QuizBrain(get_question_bank(question_data))
+
+manager = Manager()
+print(manager.question_data)
+
+quiz = QuizBrain(to_quiz_brain_data(manager.question_data))
 ui = QuizInterface(quiz)
 
-exit()
-
-while True:
-    while quiz.still_has_questions():
-        question = quiz.next_question()
-        input()
-        
-
-
-
-    print("refreshing data bank")
-    question_data = refresh_data()
-    quiz = QuizBrain(get_question_bank(question_data))
-
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
