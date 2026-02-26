6c93babb3c3927b116eb46aa05e67f82247959cd
Curt Zirzow <czirzow@gmail.com>
2026-02-25 22:46:54 -0800

    all set for news api.

--- day36/main.py
+++ day36/main.py
@@ -2,7 +2,7 @@
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-ALERT_PERCENT_CHANGE:float = 0.0
+ALERT_PERCENT_CHANGE:float = 2.0
 """percent of change needed to send an alert
 0 is every time.
 """
@@ -10,6 +10,7 @@ ALERT_PERCENT_CHANGE:float = 0.0
 
 
 # TODO: make this: apijson.ApiVantage(ApiJson): 
+import lib36.apijson as apijson
 import os
 import json
 from datetime import datetime as dt, timedelta
@@ -22,9 +23,6 @@ if not VANTAGE_API_KEY:
 VANTAGE_API_ENDPOINT = 'https://www.alphavantage.co/query'
 
 
-
-
-import lib36.apijson as apijson
 ## main
 params = {
         'function': 'TIME_SERIES_DAILY',
@@ -32,11 +30,11 @@ params = {
         'apikey': VANTAGE_API_KEY,
         }
 
-api = apijson.Request(VANTAGE_API_ENDPOINT)
-data = api.call('', params)
 
-cache_file = f"vantage-{STOCK}.json"
+cache_file = f"tmp/vantage-{STOCK}.json"
 if not os.path.exists(cache_file):
+    api = apijson.Request(VANTAGE_API_ENDPOINT)
+    data = api.call('', params=params)
     with open(cache_file, 'w') as fh:
         json.dump(data, fh, indent=4)
 else:
@@ -44,17 +42,24 @@ else:
         data = json.load(fh)
 
 
+
+## a fail safe
+TIME_SERIES_DAILY='Time Series (Daily)'
+if not TIME_SERIES_DAILY in data:
+    print("something went wrong.")
+    exit(1)
+
 # Now lets parse this data.....
 #
 DATE_FORMAT = '%Y-%m-%d'
 # {"date"}: {"awkward_names": "value" },
-time_series = data['Time Series (Daily)']
+time_series = data[TIME_SERIES_DAILY]
 """ {
         "2026-02-20": {
             "1. open": "255.1950",
             "2. high": "259.0400",
             "3. low": "253.8000",
-            "4. close": "257.1600",
+            "4. close": "257.1600",   # the value we want
             "5. volume": "4708550"
         },
     }
@@ -62,18 +67,24 @@ time_series = data['Time Series (Daily)']
 # We want the close values
 close_key = '4. close'
 # 
-today = dt.now().strftime(DATE_FORMAT)
+today:str = dt.now().strftime(DATE_FORMAT)
 if today not in time_series:
     print(f"no data for: {today}")
     exit(1)
 
 
+# TODO: create the list of dates and get the next one
+#       or just do this.... it works.
+previous_day:str = ""
+value_tdy:float = 0.0
+value_prv:float = 0.0
 try:
     days = 1
     while True:
         previous_day = (dt.now() - timedelta(days=days)).strftime(DATE_FORMAT)
         # Just in case the previous_day is a weekend or a holiday
         if previous_day in time_series:
+            """ all good, we got the value..."""
             break
         else:
             """Market was closed"""
@@ -94,11 +105,27 @@ except:
 def _get_percentage(v1, v2):
     return ((v2 - v1) / v1) * 100
 
-print(today, value_tdy, value_prv, _get_percentage(value_tdy, value_prv))
-# Just finish up.
+percentage = _get_percentage(value_tdy, value_prv)
+#debug:
+print(today, value_tdy, value_prv, percentage)
 
+# Just finish up percentage value against ALERT_PERCENT_CHANGE
+if ALERT_PERCENT_CHANGE > percentage < ALERT_PERCENT_CHANGE:
+    ## no alert needed..
+    print(f"no alert percentage[{percentage}]")
+    exit(2)
+else:
+    print(f"alert percentage[{percentage}]")
+
+
+exit(3)
 
 ## STEP 2: Use https://newsapi.org
+NEWSAPI_API_KEY = os.environ.get('VANTAGE_API_KEY')
+VANTAGE_API_ENDPOINT = 'https://www.alphavantage.co/query'
+if not NEWSAPI_API_KEY:
+    raise Exception('need to export VANTAGE_API_KEY')
+
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 ## STEP 3: Use https://www.twilio.com
