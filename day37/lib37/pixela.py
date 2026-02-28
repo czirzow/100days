966d17a871830996d2314a4010a950ff609192f3
Curt Zirzow <czirzow@gmail.com>
2026-02-27 22:25:33 -0800

    more class work.

--- day37/lib37/pixela.py
+++ day37/lib37/pixela.py
@@ -23,6 +23,7 @@ class ApiPixela():
     def __init__(self, token:str, username:str) -> None:
         self.token = token
         self.username = username
+        self.date = str(dt.now(timezone.utc).strftime('%Y%m%d'))
 
     def url_for_users(self):
         return PIXELA_URL + '/v1/users'
@@ -33,7 +34,15 @@ class ApiPixela():
     def auth_headers(self) -> dict:
         return {'X-USER-TOKEN': self.token}
 
-    def create_user(self):
+
+    def get_date(self) -> str:
+        return self.date
+
+    def set_date(self, date:str) -> None:
+        """ format: YYYYMMDD """
+        self.date = date
+
+    def create_user(self) -> str:
         # Create a user
         url = self.url_for_users()
         params = {
@@ -42,12 +51,14 @@ class ApiPixela():
                 'agreeTermsOfService': 'yes',
                 'notMinor': 'yes',
                 }
-        resp = requests.post(url=url, json=params)
-        print(resp.text)
+        return requests.post(url=url, json=params).text
 
     def create_graph(self, id:str, name:str, unit:str, type:str, color:str = 'green') -> str:
         # Create a graph
         url = self.url_for_graphs()
+
+        # TODO: make just a graph_config:dict that is passed instead of all the params
+        # move this to caller.
         graph_config = {
                         'id': id,
                         'name': name,
@@ -65,19 +76,21 @@ token = str(os.environ.get('pixela_token'))
 if username == '' or token == '':
     raise Exception("Must set the pixela_ environment vars")
 pixela = ApiPixela(token, username) 
-if 0:
+if 1:
     print(pixela.url_for_users())
     print(pixela.url_for_graphs())
+
+# test these:
+#
+#print(pixela.create_user())
+# 
+#print(pixela.create_graph())
 exit()
 
 
-
-MY_GRAPH_ID = 'graph1'
+"""
 if 0:
-
-
-MY_DATE = str(dt.now(timezone.utc).strftime('%Y%m%d'))
-if 0:
+    def add_pixel(report_data):
         # Add new pixel
         url = PIXEL_URL_GRAPHS + f"/{MY_GRAPH_ID}"
         report_data = {
@@ -88,6 +101,7 @@ if 0:
         print(resp.text)
 
 if 0:
+    def update_pixel(report_data: dict)
         # Update a pixel
         url = PIXEL_URL_GRAPHS + f"/{MY_GRAPH_ID}/{MY_DATE}"
         report_data = {
@@ -98,10 +112,11 @@ if 0:
         print(resp.text)
 
 if 0:
+    def delete_pixel():
         # Delete a pixel
         url = PIXEL_URL_GRAPHS + f"/{MY_GRAPH_ID}/{MY_DATE}"
         resp = requests.delete(url=url, headers=AUTH_HEADERS)
         print(resp.text)
 
 
-
+"""
