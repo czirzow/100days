6edbbb56d42d6fc08a41dac3d62ca5ca0e3eec98
Curt Zirzow <czirzow@gmail.com>
2026-03-02 00:24:47 -0800

    I think this class will work for what we need.

--- day39/lib39/apirequest.py
+++ day39/lib39/apirequest.py
@@ -1,40 +1,60 @@
 #filename: apirequests.py
 
 import requests as req
-class ApiRequest():
 
-    def __init__(self):
-        pass
+class ApiRequstError(Exception):
+    pass
 
-    def _request(self, method, url:str, params:dict, headers:dict) -> dict:
-        resp = method(url, params=params, headers=headers)
+class ApiRequestErrorJson(ApiRequstError):
+    pass
+
+class ApiRequestJson():
+
+    def __init__(self, url):
+        self.url = url
+
+    def _request(self, method, params:dict, headers:dict) -> dict:
+        resp = method(self.url, params=params, headers=headers)
         try:
             resp.raise_for_status()
 
         # TODO: are there specific exceptions i would like to catch? and not exit
         except req.exceptions.RequestException as e:
-            print(f"Failed request: {e}")
-            exit(1)
+            raise ApiRequstError(f"Failed request: {e}")
 
         try:
             return resp.json()
         except ValueError as e:
-            raise Exception(f"Result was not json: {e}")
+            raise ApiRequestErrorJson(f"Result was not json: {e}")
 
-    def get(self, url: str, params: dict | None = None, headers: dict | None = None) -> dict:
-        return self._request(req.get, url, params or {}, headers or {})
+    def get(self, params: dict | None = None, headers: dict | None = None) -> dict:
+        """requests.get(url, params=None, **kwargs)"""
+        return self._request(req.get, params or {}, headers or {})
 
-    def post(self, url: str, params: dict | None = None, headers: dict | None = None) -> dict:
-        return self._request(req.post, url, params or {}, headers or {})
+    def post(self, params: dict | None = None, headers: dict | None = None) -> dict:
+        """requests.post(url, data=None, json=None, **kwargs)"""
+        return self._request(req.post, params or {}, headers or {})
 
-    def put(self, url: str, params: dict | None = None, headers: dict | None = None) -> dict:
-        return self._request(req.put, url, params or {}, headers or {})
+    def put(self,  params: dict | None = None, headers: dict | None = None) -> dict:
+        """requests.put(url, data=None, **kwargs)"""
+        return self._request(req.put, params or {}, headers or {})
 
-    def delete(self, url: str, params: dict | None = None, headers: dict | None = None) -> dict:
-        return self._request(req.delete, url, params or {}, headers or {})
+    def delete(self, params: dict | None = None, headers: dict | None = None) -> dict:
+        """requests.delete(url, **kwargs)"""
+        return self._request(req.delete, params or {}, headers or {})
 
-__TEST__ = True
+
+__TEST__ = False
+__test_for__ = None
+#__test__ = 'basic-init-fail'
 if __TEST__:
-    api = ApiRequest()
-    print(api)
+    if __test_for__ == None:
+        pass
+    if __test_for__ == 'basic-init-fail':
+        #passed
+        api = ApiRequestJson('https://google.com/')
+        try:
+            resp = api.get()
+        except ApiRequstError as e:
+            print(f"we failed as expected {e}")
 
