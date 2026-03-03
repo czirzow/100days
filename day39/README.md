18f9b14b2bb35545ed12495b2610dd290e3b42ef
Curt Zirzow <czirzow@gmail.com>
2026-03-02 23:49:52 -0800

    checkin and document.

--- day39/README.md
+++ day39/README.md
@@ -2,6 +2,10 @@
 get the cheapest flight available.
 
 ### Overview
+Added the load_dotenv module, so ensure your private keys are all defined in ```.env``` file
+This file should not be committed.
+
+I believe I found a good [ApiClient](lib39/apiclient.py) to use as a base for api clients. see 
 
 ## Plan
 
@@ -11,6 +15,7 @@ get the cheapest flight available.
 
 
 ### TODO:
+* Having problems amadeus registration, revisit.
 
 
 
--- day39/main.py
+++ day39/main.py
@@ -1,48 +1,58 @@
 # day 39
 
 from flightlib.data_manager import DataManager
-#from flightlib.flight_data import FlightData
 from flightlib.flight_search import FlightSearch
 #from flightlib.notification_manager import NotificationManager
+#from flightlib.flight_data import FlightData
 
+#
+# TODO: start testing this..
 #from lib39.apirequest import ApiRequestJson
-#this fails
-#api = ApiRequestJson('http://google.com')
-#api.get()
+# it basically does a try catch for calls.
+    # this fails since it is not json.
+    # api = ApiRequestJson('http://google.com')
+    # api.get()
 
-from dotenv import load_dotenv
 import os
-from pprint import pprint
+from dotenv import load_dotenv
 from lib39.sheety import Sheety
 from lib39.cache import Cache
 
+__DEBUG__ = True
+if __DEBUG__:
+    from pprint import pprint
+
 
 load_dotenv()
-
 try: 
   sheety_key = os.environ['SHEETY_KEY']
 except KeyError:
     raise Exception("Must setup environment sheety_key")
 
-sheety = Sheety(key=sheety_key, project='flightDeals')
+
+
 
 # WARNING: do not make the SHEET plural.
 # For some reason, the api wants a singular word.
-#   sheetname == prices
-# but the value to update a sheet needs to be this:
-#    {'price': {field:value}}
+#   if sheet == prices
+#   they want this as an update value on put and post requests
+#       {'price': {field:value}}
 #  makes no sense.
 SHEET = 'price'
+sheety = Sheety(key=sheety_key, project='flightDeals')
 
 data_manager = DataManager(sheety)
+
 
 # Avoid Calls to the api.
 cache_sheety = Cache('tmp/cache_sheety_results.json')
 if cache_sheety.is_cached():
     sheety_results = cache_sheety.get()
 else:
+    #API Call
     sheety_results = data_manager.get_sheet(SHEET)
     cache_sheety.save(sheety_results)
+# call cache_sheety.clear() if data on sheety clears
 
 # ensure that we have the iata codes set.
 flight_search = FlightSearch()
@@ -51,4 +61,5 @@ for row in sheety_results[SHEET]:
     if row['iataCode'] == '':
         code = flight_search.get_iata_code(row['city'])
         data_manager.put(SHEET, row['id'], {'iataCode': code})
+
 
