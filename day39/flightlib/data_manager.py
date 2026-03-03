4e5e238940db4d0e055cf42272b7c43d4c7473f4
Curt Zirzow <czirzow@gmail.com>
2026-03-02 22:54:25 -0800

    init data_manager.

--- day39/flightlib/data_manager.py
+++ day39/flightlib/data_manager.py
@@ -1,3 +1,16 @@
+from lib39.sheety import Sheety
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self, sheety:Sheety):
+        self.sheety = sheety
+
+    def get_sheet(self, sheet):
+        return self.sheety.get(sheet)
+
+    def put(self, sheet:str, id:int, values:dict):
+        self.sheety.update(sheet, id, values)
+
+
+
+
