2ce37a4e5181c33ec923125a141be1c09a07032a
Curt Zirzow <czirzow@gmail.com>
2026-03-03 23:18:57 -0800

    progress.

--- day45/main.py
+++ day45/main.py
@@ -4,44 +4,31 @@ from mylib.cache import CacheFile
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-url = 'https://appbrewery.github.io/news.ycombinator.com/'
-cache_file = 'tmp/appbrewery.news.ycombinator.html'
+url = 'https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/'
+cache_file = 'tmp/web.archive.org.empireonline.best-movies-2.html'
 
-cache = CacheFile(cache_file)
-if cache.is_cached():
-    html = cache.get()
+# avoid calls if needed
+main_cache = CacheFile(cache_file)
+if main_cache.is_cached():
+    html = main_cache.get()
 else:
     resp = requests.get(url)
     html = resp.text
-    cache.save(html)
+    main_cache.save(html)
 
-#print(html)
-
+# clean things up for readability.. and save
 soup = BeautifulSoup(html, 'html.parser')
+cache_file = 'tmp/pretty.web.archive.org.empireonline.best-movies-2.html'
+pretty_cache = CacheFile(cache_file)
+if pretty_cache.is_cached():
+    pretty_html = pretty_cache.get()
+else:
+    pretty_html = soup.prettify()
+    pretty_cache.save(pretty_html)
 
+#print(pretty_html)
 
-# get all the scores on the sheet.
-scores_found = soup.select('span .score')
-
-# as we go through each one... lets find the winner.
-highest_score = 0
-winner_id = 0
-for score in scores_found:
-    # funky casting..
-    score_value = int(score.getText().split(' ')[0])
-    if score_value > highest_score:
-        id_value = str(score.get('id'))
-        winner_id = int(id_value[6:])
-
-print('Winner is', winner_id)
-
-# get the table row that has the information.
-winner = soup.find(name="tr", class_="athing", id=f"{winner_id}")
-a_tag = winner.select_one('a.storylink')
-
-print("The Winner:")
-print("\t", a_tag.getText())
-print("\t", a_tag.get('href'))
-
-
+soup = BeautifulSoup(pretty_html, 'html.parser')
+print("Introducting the " + str(soup.title.getText()).strip())
+print("ranked #1 to #100")
 
