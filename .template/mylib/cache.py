47cb10697b2a5507531521c3bfa392d94e95f99e
Curt Zirzow <czirzow@gmail.com>
2026-03-03 22:16:29 -0800

    sync .template with day45/mylib/cache.py

--- .template/mylib/cache.py
+++ .template/mylib/cache.py
@@ -9,6 +9,27 @@ class Cache:
     def is_cached(self) -> bool:
         return os.path.exists(self.cache_file)
 
+    def clear(self):
+        """clears the cache"""
+        if self.is_cached():
+            os.remove(self.cache_file)
+
+class CacheFile(Cache):
+    """Just save the contents to a file"""
+
+    def get(self) -> str:
+        with open(self.cache_file, 'r') as fp:
+            return fp.read()
+
+    def save(self, data) -> None:
+        with open(self.cache_file, 'w') as fp:
+            fp.write(data)
+
+
+# untested
+class CacheJson(Cache):
+    """Use json to save the file"""
+
     def get(self) -> dict:
         with open(self.cache_file, 'r') as fp:
             return json.load(fp)
@@ -17,8 +38,4 @@ class Cache:
         with open(self.cache_file, 'w') as fp:
             json.dump(fp=fp, obj=results, indent=4)
 
-    def clear(self):
-        """clears the cache"""
-        if self.is_cached():
-            os.remove(self.cache_file)
 
