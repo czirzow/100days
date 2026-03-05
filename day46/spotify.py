e9ea0eefab518448ff0b390835107d42ba6ed076
Curt Zirzow <czirzow@gmail.com>
2026-03-04 22:10:43 -0800

    Plans for integrating spotipy api

---
+++ day46/spotify.py
@@ -0,0 +1,17 @@
+import spotipy
+
+# TODO:
+# get playlist
+# save play liist info to file.
+
+
+# create sublists:
+#   ska like
+#   punk like
+#   [genre like items]
+# save into a file  in a spotiy format.
+
+
+# read saved list
+# put save list to spotify
+
