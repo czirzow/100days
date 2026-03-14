f9a0374ed0366f6fe62dca8ee12e854ec9d8883b
Curt Zirzow <czirzow@gmail.com>
2026-03-13 21:07:54 -0700

    60 per second :/

--- day48/cookie.py
+++ day48/cookie.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from time import sleep, time
 
 # debug...
 from pprint import pprint
+
 
 class CookieMonster:
     """He does just gobble up cookies
@@ -15,6 +16,8 @@ class CookieMonster:
     def __init__(self, driver, lang='DE'):
         self.driver = driver
         self.lang = lang
+        self.cookies = 0
+        self.cookies_per_sec = 0.0
         try:
             # I cheated on this, that damn langSlect-EN would never work.
             # it was all about the sleeping
@@ -30,28 +33,44 @@ class CookieMonster:
 
     def num_cookies(self):
         cookies = self.driver.find_element(by=By.ID, value="cookies")
-        return int(cookies.text.split(' ')[0].replace(',', ''))
+        self.cookies = int(cookies.text.split(' ')[0].replace(',', ''))
+        return self.cookies
+
+    def get_per_second(self):
+        # this item isn't always ready and can be missing.
+        per_sec = self.driver.find_element(by=By.ID, value="cookiesPerSecond")
+        try:
+            self.cookies_per_sec = float(per_sec.text.replace('per second: ', '').replace(',',''))
+        except StaleElementReferenceException:
+            """Just ignore this.. we will get it at some point."""
+            pass
+
+        return self.cookies_per_sec
+
+    def get_products(self):
+        return self.driver.find_elements(by=By.CSS_SELECTOR, value=".product.unlocked.enabled")
+
+    def get_upgrades(self):
+        return self.driver.find_elements(by=By.CSS_SELECTOR, value=".crate.upgrade.enabled")
+        
 
     # general names of action here:
     def eat(self, cookie):
         cookie.click()
 
-    def gobble(self, qty:int = 356):
+    def gobble(self, seconds:float = 1.0):
         # I only get this here cause i'm not sure if it changes
         cookie = self.get_cookie()
-        i = 0
-        while qty > 0:
+        start = time()
+        while time() - start <= seconds:
             self.eat(cookie)
-            qty -= 1
-            i += 1
             # DEBUG: 
-            if i % 100 == 0:
-                pprint(self.num_cookies())
-            sleep(0.003) # lets breath a moment.
+            #sleep(0.003) # lets breath a moment.
 
 
 
 # Main
+
 
 url = 'https://ozh.github.io/cookieclicker/'
 
@@ -67,10 +86,38 @@ Sid = CookieMonster(driver, lang='EN')
 
 # FIXME: I believe we can figure out a state on when to continue instead of sleeping
 # FIXME: We have to wait for javascript to set things up.0
-sleep(3) # wait for javascript.
+sleep(2) # wait for javascript.
 
+run_for = 300
+start = time()
+
+our_per_second = 24.0
 cookies_to_eat = 15
-Sid.gobble(cookies_to_eat)
+while time() - start <= run_for:
+    time_to_eat = our_per_second / cookies_to_eat
+    Sid.gobble(time_to_eat)
+
+    upgrades = Sid.get_upgrades()
+    for upgrade in upgrades:
+        try:
+            upgrade.click()
+            print('upgrade')
+        except StaleElementReferenceException:
+            """Just ignore this.. we will get it at some point."""
+            print('missed upgrade')
+            pass
+
+    products = Sid.get_products()
+    for product in products[::-1]:
+        product.click()
+        cookies_to_eat = int(product.find_element(By.CLASS_NAME, value="price").text.replace(',', ''))
+        print(cookies_to_eat, Sid.get_per_second())
+
+
+    #sleep(0.5)
+    #product.click()
+
+
 # Now... lets start getting the upgrades to increase per Sec.
 # buy a click. at 15.
 #    recalculate cookies_to_eat to the next level.
