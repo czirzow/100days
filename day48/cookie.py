8bb8f149954736802e958408c9661003cdf597bb
Curt Zirzow <czirzow@gmail.com>
2026-03-15 22:28:24 -0700

    having a WS issue with editor.. checkin.

--- day48/cookie.py
+++ day48/cookie.py
@@ -2,46 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+#TODO: add the needed type casting abilities
+
 from time import sleep, time
 
-# debug...
+# DEBUG: just in case... it helps.
 from pprint import pprint
 
 
-class CookieMonster:
-    """He does just gobble up cookies
-    The more cookies you eat the more you make.
-    """
+# Indentation test.
+class Asdf():
+    def __init__(self):
+        pass
+
+class SidsBrain:
 
     def __init__(self, driver, lang='DE'):
-        self.driver = driver
-        self.lang = lang
-        self.cookies = 0
-        self.cookies_per_sec = 0.0
-        try:
-            # I cheated on this, that damn langSlect-EN would never work.
-            # it was all about the sleeping
-            sleep(3)
-            language_button = self.driver.find_element(by=By.ID, value=f"langSelect-{self.lang}")
-            language_button.click()
-        except NoSuchElementException:
-            print("Language selection not found")
+
+       self.driver = driver
+       self.lang = lang
+       self.cookies = 0
+       self.cookies_per_sec = 0.0
+       try:
+           # I cheated on this, that damn langSlect-EN would never work.
+           # it was all about the sleeping
+           sleep(3)
+           language_button = self.driver.find_element(by=By.ID, value=f"langSelect-{self.lang}")
+           language_button.click()
+       except NoSuchElementException:
+           print("Language selection not found")
 
     def get_cookie(self):
-        # I'm not sure if this changes... all other elements do.
+       # I'm not sure if this changes... all other elements do.
         return self.driver.find_element(by=By.ID, value="bigCookie")
 
     def num_cookies(self):
-        try:
-            cookies = self.driver.find_element(by=By.ID, value="cookies")
-            self.cookies = int(cookies.text.split(' ')[0].replace(',', ''))
-        except StaleElementReferenceException:
-            """Just ignore this.. we will get it at some point."""
-            pass
-        return self.cookies
+       try:
+           cookies = self.driver.find_element(by=By.ID, value="cookies")
+           self.cookies = int(cookies.text.split(' ')[0].replace(',', ''))
+       except StaleElementReferenceException:
+           """Just ignore this.. we will get it at some point."""
+           pass
+       return self.cookies
 
     def get_per_second(self):
-        # this item isn't always ready and can be missing.
+       # this item isn't always ready and can be missing.
         per_sec = self.driver.find_element(by=By.ID, value="cookiesPerSecond")
         try:
             self.cookies_per_sec = float(per_sec.text.replace('per second: ', '').replace(',',''))
@@ -51,15 +56,34 @@ class CookieMonster:
         return self.cookies_per_sec
 
     def get_products(self):
-        return self.driver.find_elements(by=By.CSS_SELECTOR, value=".product.unlocked.enabled")
+       return self.driver.find_elements(by=By.CSS_SELECTOR, value=".product.unlocked.enabled")
 
     def get_upgrades(self):
-        return self.driver.find_elements(by=By.CSS_SELECTOR, value=".crate.upgrade.enabled")
-        
+       return self.driver.find_elements(by=By.CSS_SELECTOR, value=".crate.upgrade.enabled")
+
+
+class CookieMonster(SidsBrain):
+    """He does just gobble up cookies
+    The more cookies you eat the more you make.
+    """
+    def __init__(self, driver, **kwargs):
+        """
+         driver: a webdriver from selenium.
+        """
+        # FIXME: not sure if kwargs is done properly. test.
+        super().__init__(driver, *kwargs)
+        self.cookie = self.get_cookie()
 
     # general names of action here:
-    def eat(self, cookie):
-        cookie.click()
+    def _eat(self):
+       """ eat a cookie """
+       self.cookie.click()
+
+    def open_cookie_jar(self) -> tuple:
+        return(0, 0)
+
+    def want_more_cookies(self):
+        pass
 
     def gobble(self, how_many: int = 100, for_how_long: float = 5.0):
         """
@@ -74,18 +98,16 @@ class CookieMonster:
         """
 
         # I only get this here cause i'm not sure if it changes
-        cookie = self.get_cookie()
         start = time()
         while how_many > 0:
             if time() - start >= for_how_long:
                 # no need to continue
                 return
             how_many -= 1
-            self.eat(cookie)
+            self._eat()
 
             # DEBUG: 
             #sleep(0.003) # lets breath a moment.
-
 
 
 # Main
@@ -100,59 +122,63 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
 
 
-# Sid: The original name for Cookie Monster by Jim Henson in the late 1960's
 Sid = CookieMonster(driver, lang='DE')
 
 # FIXME: I believe we can figure out a state on when to continue instead of sleeping
 # FIXME: We have to wait for javascript to set things up.0
 sleep(2) # wait for javascript.
 
-run_for = 600
+RUN_FOR = 600 # seconds
 start = time()
 
+# just a caclution I did that we can do with just clicking the cookie.
 our_per_second = 24.0
+
 cookies_to_eat = 15
-while time() - start <= run_for:
-    elapsed_time = round(time() - start)
+while time() - start <= RUN_FOR:
+    _elapsed_time = round(time() - start)
     
-    total_rate = round(our_per_second + Sid.get_per_second(), 2)
-    estimated_time = round(cookies_to_eat/total_rate, 2)
-    time_to_eat = estimated_time #( Sid.get_per_second()) / cookies_to_eat
-    print(elapsed_time, Sid.num_cookies(), f"eat {cookies_to_eat} for {time_to_eat}")
-    print(Sid.get_per_second(), total_rate, estimated_time)
+    if False:
+        (cookies_to_eat, time_to_eat) = Sid.open_cookie_jar()
+    else:
+        # rethink and move to open_cookie_jar)()
+        total_rate = round(our_per_second + Sid.get_per_second(), 2)
+        estimated_time = round(cookies_to_eat/total_rate, 2)
+        time_to_eat = estimated_time #( Sid.get_per_second()) / cookies_to_eat
+        print(_elapsed_time, Sid.num_cookies(), f"eat {cookies_to_eat} for {time_to_eat}")
+        print(Sid.get_per_second(), total_rate, estimated_time)
 
     Sid.gobble(how_many=cookies_to_eat, for_how_long=time_to_eat)
 
-    upgrades = Sid.get_upgrades()
-    for upgrade in upgrades[::-1]:
-        try:
-            upgrade.click()
-            print('upgrade')
+    if False:
+        Sid.want_more_cookies()
+    else:
+        # move to want_more_cookies()
+        upgrades = Sid.get_upgrades()
+        for upgrade in upgrades[::-1]:
+            try:
+                upgrade.click()
+                print('upgrade')
+                break
+            except StaleElementReferenceException:
+                """Just ignore this.. we will get it at some point."""
+                print('missed upgrade')
+                pass
+
+        products = Sid.get_products()
+        for product in products[::-1]:
+            product.click()
+            # TODO: rework the logic on what number to get next.
+            #       consider how many cookies that still exist to be eaten.
+            cookies_to_eat = int(product.find_element(By.CLASS_NAME, value="price").text.replace(',', ''))
             break
-        except StaleElementReferenceException:
-            """Just ignore this.. we will get it at some point."""
-            print('missed upgrade')
-            pass
-
-    products = Sid.get_products()
-    for product in products[::-1]:
-        product.click()
-        cookies_to_eat = int(product.find_element(By.CLASS_NAME, value="price").text.replace(',', ''))
-        break
-    # in case we have access of cookies
-    #cookies_to_eat -= Sid.num_cookies()
+        # in case we have access of cookies
+        #cookies_to_eat -= Sid.num_cookies()
 
 
-    #sleep(0.5)
-    #product.click()
 print("Cookies per second", Sid.get_per_second())
 Sid.get_per_second()
 
-# Now... lets start getting the upgrades to increase per Sec.
-# buy a click. at 15.
-#    recalculate cookies_to_eat to the next level.
-# get what is available and upgrade top down.
-# or bottom up? who knows.
 
 
 
