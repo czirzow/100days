905907524750d25d6d16cd41aed4dc9c59fe4164
Curt Zirzow <czirzow@gmail.com>
2026-03-18 21:55:34 -0700

    initial notes.

--- day55/notes.py
+++ day55/notes.py
@@ -1,2 +1,20 @@
 # notes
 # Guess the number website.
+
+#https://localhost/uri
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world.'
+
+@app.route('/bye')
+def bye():
+    return 'Thanks for all the fish.'
+
+
+if __name__ == 'main':
+    app.run()
