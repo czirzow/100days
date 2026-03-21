d47afb2329e4bb3db0775bd133b7f413b5f62cfb
Curt Zirzow <czirzow@gmail.com>
2026-03-21 13:33:52 -0700

    copyright.

--- day57/server.py
+++ day57/server.py
@@ -4,12 +4,14 @@
 #https://localhost/uri
 
 from flask import Flask, render_template
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    year = date.today().year
+    return render_template('index.html', year=year)
 
 if __name__ == '__main__':
     app.run(debug=True)
