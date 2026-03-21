b45847a95845596eae1aa8c587f57719fe2a9b19
Curt Zirzow <czirzow@gmail.com>
2026-03-21 13:14:53 -0700

    well...

--- day56/main.py
+++ day56/main.py
@@ -1,1 +1,15 @@
-# day 
+# notes
+# Guess the number website.
+
+#https://localhost/uri
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
