2caf58bbfd2df7f6f7b52c5e0dfd47160a0b749f
Curt Zirzow <czirzow@gmail.com>
2026-03-21 14:15:57 -0700

    Add the guessing of name and age.

--- day57/server.py
+++ day57/server.py
@@ -5,12 +5,26 @@
 
 from flask import Flask, render_template
 from datetime import date
+from mylib.guess import Guess
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html', today=date.today())
+
+@app.route('/guess/<name>')
+def guess(name):
+    api = Guess(name=name)
+    return render_template('guess.html',
+                           name=name,
+                           age=api.guess_age(),
+                           gender=api.guess_gender(),
+                           today=date.today()
+                           )
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
